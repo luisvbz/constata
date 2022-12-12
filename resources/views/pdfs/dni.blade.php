ca4dc2346a21ab8bdbf0ea41e15a5c6bc429f54e
Carlos Meneses <osen.10112@gmail.com>
2022-12-12 00:28:37 -0500

    Create dni.blade.php

---
+++ resources/views/pdfs/dni.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        @page {
+            size: auto;
+        }
+    </style>
+</head>
+<body>
+    <img src="{{ resource_path('images/diseño-dni-front.png') }}" width="321.260px" alt="front">
+    <div style="color:#912330;width:200px;position:fixed;top:6.5;left:248;font-size:7.5pt;font-weight:bold;">70035765</div>
+    <div style="width:20px;position:fixed;top:6.5;left:302;font-size:7.5pt;font-weight:bold;">- 7</div>
+    <!-- Datos -->
+    <div style="position:fixed;top:19;left:85;font-size:6pt;">Primer Apellido</div>
+    <div style="position:fixed;top:27;left:85;font-weight:bold;font-size:6pt;">CUADROS</div>
+    <div style="position:fixed;top:40;left:85;font-size:6pt;">Segundo Apellido</div>
+    <div style="position:fixed;top:48;left:85;font-weight:bold;font-size:6pt;">GONZALES</div>
+    <div style="position:fixed;top:60;left:85;font-size:6pt;">Pre Nombres</div>
+    <div style="position:fixed;top:68;left:85;font-weight:bold;font-size:6pt;">MIGUEL ANTONIO</div>
+    <div style="position:fixed;top:80;left:85;font-size:6pt;">Nacimiento: Fecha y Ubigeo</div>
+    <div style="position:fixed;top:89;left:85;font-weight:bold;font-size:6pt;">08 10 1994</div>
+    <div style="position:fixed;top:89;left:140;font-weight:bold;font-size:6pt;text-align: center;">240104</div>
+    <div style="position:fixed;top:102;left:85;font-size:6pt;">Sexo</div>
+    <div style="position:fixed;top:102;left:130;font-size:6pt;">Estado Civil</div>
+    <div style="position:fixed;top:112;left:85;font-weight:bold;font-size:6pt;">M</div>
+    <div style="position:fixed;top:112;left:130;font-weight:bold;font-size:6pt;text-align: center;">S</div>
+    <div style="width:75px;position:fixed;top:115;left:10;color:#912330;text-align:center;font-size:6pt;letter-spacing:0.1;">CUADROS</div>
+    <div style="width:100px;position:fixed;rotate:90;top:21;left:3;color:#912330;letter-spacing:5;font-weight:bold;font-size:8.5pt;">70035765</div>
+
+    <div style="width:150;position:fixed;top:21;left:240;">
+        <table style="font-size:6pt;border-collapse:collapse;" cellspacing="0" cellpadding:0;>
+            <tr>
+                <td align="center" style="border:1px solid #0f0f0f;padding:3px 5px;">
+                    <div>Fecha Inscripción</div>
+                    <div style="font-weight:bold;">20 05 2004</div>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" style="border:1px solid #0f0f0f;padding:3px 5px;">
+                    <div>Fecha Emisión</div>
+                    <div style="font-weight:bold;">20 05 2004</div>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" style="border:1px solid #0f0f0f;padding:3px 5px;">
+                    <div>Fecha Caducidad</div>
+                    <div style="font-weight:bold;color:#912330;">20 05 2004</div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div style="width:305px;position:fixed;top:126;left:10;font-size:10pt;font-family:monospace;font-weight:bold;letter-spacing:2.1;line-height:1.2;">
+        <p>{{ 'I<PER70035765<9<<<<<<<<<<<<<<<9410084M3004054PER<<<<<<<<<<<4CUADROS<<MIGUEL<ANTONIO<<<<<<<' }}</p>
+    </div>
+
+    <pagebreak type="NEXT-EVEN" />
+
+    <img src="{{ resource_path('images/diseño-dni-post.png') }}" width="321.260px" alt="front">
+    <div style="width:230px;position:fixed;top:6;left:8;">
+        <table style="width:100%;font-size:5.5pt;border-collapse:collapse;" cellspacing="0" cellpadding:0;>
+            <tr>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+                <td align="center" style="border:1px solid #00377E;padding:4px 6px;">
+                    <div style="font-weight:bold;">CONSTANCIA DE SUFRAGIO</div>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div style="position:fixed;top:73;left:10;font-size:6pt;">Departamento</div>
+    <div style="position:fixed;top:81;left:10;font-weight:bold;font-size:6pt;">LIMA</div>
+    <div style="position:fixed;top:73;left:90;font-size:6pt;">Provincia</div>
+    <div style="position:fixed;top:81;left:90;font-weight:bold;font-size:6pt;">LIMA</div>
+    <div style="position:fixed;top:73;left:170;font-size:6pt;">Distrito</div>
+    <div style="position:fixed;top:81;left:170;font-weight:bold;font-size:6pt;">SAN MARTIN DE PORRES</div>
+    <div style="position:fixed;top:95;left:10;font-size:6pt;">Dirección</div>
+    <div style="position:fixed;top:103;left:10;font-weight:bold;font-size:6pt;">MZ A LT 20 URB LOS DOMINIOS</div>
+    <div style="position:fixed;top:115;left:10;font-size:6pt;">Observaciones</div>
+    <div style="position:fixed;top:122;left:10;font-size:6pt;">Donación de Organos</div>
+    <div style="width:100px;position:fixed;top:122;left:90;font-weight:bold;font-size:6pt;">SI</div>
+    <div style="position:fixed;top:122;left:126;font-size:6pt;">Grupo de Votación</div>
+    <div style="width:100px;position:fixed;top:122;left:195;font-weight:bold;font-size:6pt;">260101</div>
+ 
+</body>
+</html>
