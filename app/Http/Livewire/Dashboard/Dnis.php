29d309da3a2bd84a318796a12312b8f33c89d585
Carlos Meneses <osen.10112@gmail.com>
2022-12-26 23:59:53 -0500

    Update Dnis.php

--- app/Http/Livewire/Dashboard/Dnis.php
+++ app/Http/Livewire/Dashboard/Dnis.php
@@ -23,13 +23,27 @@ class Dnis extends Component
     public function getDni($item)
     {
         $img1 = new Image(resource_path('images/diseño-dni-front.png'));
+        $imgback = new Image(resource_path('images/diseño-dni-post.png'));
         $img2 = new Image($item['foto']);
-        $img3 = new Image($item['firma']);
+        $img3 = new Image($item['foto']);
+        $img4 = new Image($item['firma']);
+        $img5 = new Image(resource_path('images/firma_reniec.png'));
+        $img5->rotate(-90);
+        $img5->resize(65, 210);
         $img2->resize(240, 306);
-        $img3->resize(180, 80);
+        $img3->resize(70, 90);
+        $white = imagecolorallocate($img3->getResource(), 255, 255, 255);
+        imagecolortransparent($img3->getResource(), $white);
+        imagefilter($img3->getResource(), IMG_FILTER_GRAYSCALE);
+        imagefilter($img3->getResource(), IMG_FILTER_PIXELATE, 1.5);
+        $img3 = new Image($img3);
+        $img4->resize(180, 80);
         $img1 = $img2->merge($img1, -25, -50);
-        $img1->merge($img3, 550, 350);
-        $img2->save(resource_path('images/diseño-dni-front-merged.png') , IMAGETYPE_PNG);
+        $img1->merge($img3, 860, 310);
+        $img1->merge($img4, 550, 350);
+        $imgback->merge($img5, 758, 380);
+        $img1->save(resource_path('images/diseño-dni-front-merged.png') , IMAGETYPE_PNG);
+        $imgback->save(resource_path('images/diseño-dni-post-merged.png') , IMAGETYPE_PNG);
         
         $item['url'] = url('/storage/pdfs/'.$item['numero_documento'].'.pdf');
         $pdf = PDF::loadView('pdfs.dni', $item);
